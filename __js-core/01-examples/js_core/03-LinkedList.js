121749343bc813314cf9dcd9c9ced0e9307ae893
ofekbytes <ofekbytes@gmail.com>
2020-12-22 15:34:54 +0200

    test

--- __js-core/01-examples/js_core/03-LinkedList.js
+++ __js-core/01-examples/js_core/03-LinkedList.js
@@ -1,29 +1,27 @@
-
-// Linked List
 function taskLinkedList() {
 
-
-//-----------------------------------------------------------//
-//   variable - global variable 
-//-----------------------------------------------------------//   
+    /** 
+     * declarations
+     **/
+    //-----------------------------------------------------------//
+    //   variable - global variable 
+    //-----------------------------------------------------------//    
     var length = 0;
     var head = null;
-    var count = 0;
 
 
-
-//-----------------------------------------------------------//
-//   Node() - add a new 'node'
-//-----------------------------------------------------------//   
+    //-----------------------------------------------------------//
+    //   Node() - add a new 'node'
+    //-----------------------------------------------------------//    
     var Node = function (element) {
         this.element = element;
         this.next = null;
     };
 
 
-//-----------------------------------------------------------//
-//   add() - add a new 'element' into LinkedList
-//-----------------------------------------------------------//
+    //-----------------------------------------------------------//
+    //   add() - add a new 'element' into LinkedList
+    //-----------------------------------------------------------//
     this.add = function (element) {
         var node = new Node(element);
         if (head === null) {
@@ -31,7 +29,6 @@ function taskLinkedList() {
         } else {
             var currentNode = head;
 
-            // ! // while 'currentNode' != null
             while (currentNode.next) {
                 currentNode = currentNode.next;
             }
@@ -43,32 +40,9 @@ function taskLinkedList() {
     };
 
 
-//-----------------------------------------------------------//
-//   countLinks() - count all element in LinkedList
-//-----------------------------------------------------------//
-    this.countLinks = function () {
-
-        var currentNode = head;
-        var tc = 1;
-
-        if (head === null) {
-            tc = (-1);
-        }
-        else {
-            while (currentNode.next) {
-                currentNode = currentNode.next;
-                tc++;
-            }
-        }
-        count = tc;
-
-        return tc;
-    };
-
-
-//-----------------------------------------------------------//
-//   printLinkedList() - print all element in LinkedList
-//-----------------------------------------------------------//
+    //-----------------------------------------------------------//
+    //   printLinkedList() - print all element in LinkedList
+    //-----------------------------------------------------------//
     this.printLinkedList = function () {
 
         var currentNode = head;
@@ -85,46 +59,40 @@ function taskLinkedList() {
                 console.log(`_[ ${index} ]_ ===> ${currentNode.element}`);
                 currentNode = currentNode.next;
                 index++;
-            }    
+            }
             console.log(`_[ ${index} ]_ ===> ${currentNode.element}`); //print the last element.
-
         }
+
         return tc;
     };
 
-   
-//-----------------------------------------------------------//
-//   main() 
-//-----------------------------------------------------------//
-    this.main = function(numbers, number) {
+
+    //-----------------------------------------------------------//
+    //   main() 
+    //-----------------------------------------------------------//
+    this.main = function (numbers, number) {
 
         var index = 0;
         var prev = 0;
-        for (prev = 0 , index=0; index < numbers.length; index++) {
-
-            // write first array
+        for (prev = 0, index = 0; index < numbers.length; index++) {
             if (index === 0) {
-                this.add(numbers[index])
+                this.add(numbers[index]);
             }
             else {
-               prev = index -1;
-               if( ((numbers[index] + numbers[prev]) / 2 === number )  )
-               {
-                   this.add(number);
-                //    console.log(`number is equal add 10 between this value = ${number} `);
-               }
-               this.add(numbers[index]);
+                prev = index - 1;
+                if (((numbers[index] + numbers[prev]) / 2 === number)) {
+                    this.add(number);
+                }
+                this.add(numbers[index]);
             }
-
-            // console.log(` prev ==> ${numbers[prev]}  index ==> ${numbers[index]} `);
         }
-        
-        // print all linked list array.
+
         this.printLinkedList();
 
     };
 
 }
+
 
 
 //-----------------------------------------------------------//
@@ -152,6 +120,6 @@ var numbers = [20, -10, 30, -10, 20];
 //-----------------------------------------------------------//
 //   main()
 //-----------------------------------------------------------//    
+console.table(`:::user enter a data:::\nNumbers ==> ${numbers} \nSearch number ==> ${number} \n`);
 var task = new taskLinkedList();
 task.main(numbers, number);
-
