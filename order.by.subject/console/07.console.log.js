c725525a5ffd37d7b58adf47cc0f3eccd3ef198e
jron <yaron.kessler41@gmail.com>
2021-05-16 11:53:24 +0300

    test

--- order.by.subject/console/07.console.log.js
+++ order.by.subject/console/07.console.log.js
@@ -7,6 +7,8 @@ const rec1 = { name: 'shalom', age: 30, state: 'usa', single: false };
 const rec2 = { name: 'yifat', age: 40, state: 'il', single: false };
 const rec3 = { name: 'yaron', age: 46, state: 'il', single: true };
 
+console.log('\n\n%c___console.table___', 'color: red; font-weight: bold');
+
 
 console.table({ rec1, rec2, rec3 });
 
